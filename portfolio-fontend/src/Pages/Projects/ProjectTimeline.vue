48d82c7eb9c9250032d1e1539f7b85494eaa4206
popivn <popixworkspace@gmail.com>
2025-02-19 16:06:48 +0700

    Fix project section reponsive

--- portfolio-fontend/src/Pages/Projects/ProjectTimeline.vue
+++ portfolio-fontend/src/Pages/Projects/ProjectTimeline.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="timeline space-y-6 relative">
-        <div class="absolute left-[22%] transform -translate-x-1/2 top-0 h-full w-[2px] bg-primary"></div>
+        <div class="absolute left-[28%] sm:left-1/4 transform -translate-x-1/2 top-0 h-full w-[2px] bg-primary"></div>
 
         <div v-for="(project, index) in projects" :key="index" class="event flex items-start gap-6 relative">
-            <div class="timestamp w-40 text-right font-bold text-primary">
+            <div class="timestamp text-right font-bold text-primary">
                 {{ project.date }}
             </div>
-            <div class="w-4 h-4 bg-secondary rounded-full absolute left-[22%] transform -translate-x-1/2"></div>
+          <div class="w-4 h-4 bg-secondary rounded-full absolute left-[28%] sm:left-1/4  transform -translate-x-1/2"></div>
 
-            <div class="project flex-1 p-6 pt-0 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+            <div class="ml-2 sm:ml-[14%] project flex-1 p-6 pt-0 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
                 <h3 class="text-xl text-primary mb-4 font-bold">{{ project.name }}</h3>
                 <p class="text-sm text-secondary">{{ project.description }}</p>
             </div>
