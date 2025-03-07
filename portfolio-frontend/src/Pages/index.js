46d1fa663a52778e96f9cd5b63562e83c359134f
popivn <popixworkspace@gmail.com>
2025-03-08 02:33:00 +0700

    --no-edit

--- portfolio-frontend/src/Pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AboutPage } from './About/Index.vue';
-export { default as ProjectsPage } from './Projects/Index.vue';
-export { default as ExperiencePage } from './Experience/Index.vue';
-export { default as PostsPage } from './Posts/Index.vue';
--- portfolio-frontend/vite.config.ts
+++ portfolio-frontend/vite.config.ts
@@ -1,33 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-    process.env.NODE_ENV === 'development' ? vueDevTools() : null,
-  ].filter(Boolean),  
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  
+  return {
+    base: './',
+    plugins: [
+      vue(),
+      vueJsx(),
+      isDev ? vueDevTools() : null,
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true,  
-      interval: 100,     
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode)
     },
-    hmr: {
-      clientPort: 5173,
-      host: 'localhost' 
-    },
-    allowedHosts: ['popivn.onrender.com'],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+      hmr: {
+        clientPort: 5173,
+        host: 'localhost'
+      },
+      allowedHosts: ['popivn.onrender.com'],
+    }
   }
 })
