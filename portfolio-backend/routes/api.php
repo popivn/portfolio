e7e748731898aaa30513dcadcb6c48b3ac7a1b16
popivn <popixworkspace@gmail.com>
2025-03-10 05:03:32 +0700

    fix CRFS token

--- portfolio-backend/routes/api.php
+++ portfolio-backend/routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProjectController;
 use App\Http\Controllers\API\UserDetailController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use App\Http\Controllers\API\ContactController;
 
 Route::prefix('api')->withoutMiddleware([VerifyCsrfToken::class])->group(function () {
     Route::get('projects', [ProjectController::class, 'getAllProject']);
     Route::get('user-detail', [UserDetailController::class, 'getFirstUserDetail']);
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 });
--- portfolio-backend/routes/web.php
+++ portfolio-backend/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserDetailController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ContactController;
 use App\Http\Controllers\NotificationController;
 
 include('api.php');
@@ -33,12 +32,9 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/notifications', [NotificationController::class, 'getNotifications'])->name('notifications.get');
 
-// Route::get('/notifications/mark-as-read/{id}', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
-// Route::get('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead'])->name('notifications.markAllAsRead');
+Route::get('/notifications/mark-as-read/{id}', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+Route::get('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead'])->name('notifications.markAllAsRead');
 
-Route::post('/contact', [ContactController::class, 'store'])
-    ->name('contact.store')
-    ->middleware(['App\Http\Middleware\CorsMiddleware']);
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
     Route::get('/contacts', 'App\Http\Controllers\Admin\ContactController@index')->name('admin.contacts.index');
     Route::get('/contacts/{id}', 'App\Http\Controllers\Admin\ContactController@show')->name('admin.contacts.show');
